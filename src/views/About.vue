1568c7c1598514df7c39f0740f8ab8c5b2bceda1
Ray0Emma <ahmadifarah22@gmail.com>
2022-01-07 21:08:58 +0000

    feat: Configuring tailwind

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
+  <div class="about mb-4 text-3xl font-bold text-gray-600 underline">
     <h1>This is an about page</h1>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
