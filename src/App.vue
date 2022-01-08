b0f201d55359ffae59597fc023be11c9c8eebfd9
Ray0Emma <ahmadifarah22@gmail.com>
2022-01-08 12:57:23 +0000

    feat: added navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
+  <Navbar />
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view />
 </template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+export default {
+  name: "App",
+  components: {
+    Navbar,
+  },
+};
+</script>
 
 <style>
 #app {
---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bg-white shadow-sm">
+    <div class="max-w-6xl mx-auto px-4">
+      <!-- Website Logo -->
+      <div class="flex justify-center py-6 px-2">
+        <router-link to="/">
+          <img
+            src="../assets/img/logo/torii-gate.png"
+            alt="torii-gate logo"
+            class="h-8 w-8 mr-2 inline"
+          />
+          <span class="font-semibold text-gray-700 text-lg">AniGeeks</span>
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
