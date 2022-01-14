9f3a5111ee6ae6ab9c05c95a5346aaa60418433c
Ray0Emma <ahmadifarah22@gmail.com>
2022-01-14 20:25:47 +0000

    feat: added searchBar component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <Navbar />
-  <div id="nav">
-    <!-- <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> -->
-  </div>
+  <main class="container mx-auto px-8">
+    <SearchBar />
+  </main>
   <router-view class="container mx-auto px-8" />
   <Footer />
 </template>
@@ -11,10 +10,12 @@
 <script>
 import Navbar from "./components/Navbar.vue";
 import Footer from "./components/Footer.vue";
+import SearchBar from "./components/main/SearchBar.vue";
 export default {
   name: "App",
   components: {
     Navbar,
+    SearchBar,
     Footer,
   },
 };
---
+++ src/components/main/SearchBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <form action="">
+      <input type="text" placeholder="Type your anime name here" />
+      <button
+        class="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-red-500 hover:bg-red-700"
+      >
+        Search
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
