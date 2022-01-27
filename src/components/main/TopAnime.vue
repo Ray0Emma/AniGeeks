f72c53b5fe41724969ee4de13b12856423842780
Ray0Emma <ahmadifarah22@gmail.com>
2022-01-27 21:56:06 +0000

    feat: added averageScore

--- src/components/main/TopAnime.vue
+++ src/components/main/TopAnime.vue
@@ -37,6 +37,7 @@ export default {
                       romaji
                       english
                     }
+                    averageScore
                     siteUrl
                     isAdult
                     coverImage {
