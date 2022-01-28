2694510f883edf99d7f6578cf7068237b151b678
Ray0Emma <ahmadifarah22@gmail.com>
2022-01-28 10:25:16 +0000

    feat: added topManga section

--- src/components/main/TopManga.vue
+++ src/components/main/TopManga.vue
@@ -5,50 +5,66 @@
       Top <span class="text-red-700">6</span> Manga
     </h5>
     <p class="text-gray-400 mt-4">Based on users rating</p>
-    <Card :medias="medias" />
+    <Card :medias="topManga" />
   </div>
 </template>
 
 <script>
 import { Icon } from "@iconify/vue";
 import Card from "./Card.vue";
+import apiData from "../../sevices/apiData";
 export default {
+  name: "TopManga",
   data() {
     return {
-      name: "TopManga",
-      medias: [
-        {
-          title: "Hunter x Hunter",
-          src: "",
-          rating: "9,96",
-        },
-        {
-          title: "Hunter x Hunter",
-          src: "src/assets/img/hero.jpg",
-          rating: "9,96",
-        },
-        {
-          title: "Hunter x Hunter",
-          src: "src/assets/img/hero.jpg",
-          rating: "9,96",
-        },
-        {
-          title: "Hunter x Hunter",
-          src: "src/assets/img/hero.jpg",
-          rating: "9,96",
-        },
-        {
-          title: "Hunter x Hunter",
-          src: "src/assets/img/hero.jpg",
-          rating: "9,96",
-        },
-        {
-          title: "Hunter x Hunter",
-          src: "src/assets/img/hero.jpg",
-          rating: "9,96",
-        },
-      ],
+      topManga: [],
     };
+  },
+
+  async mounted() {
+    let query = `
+          query ($page: Int, $perPage: Int) {
+              Page(page: $page, perPage: $perPage) {
+                pageInfo {
+                  total
+                  currentPage
+                  lastPage
+                  hasNextPage
+                  perPage
+                }
+                  media(type: MANGA, sort: TRENDING_DESC) {
+                    id
+                    title {
+                      romaji
+                      english
+                    }
+                    averageScore
+                    siteUrl
+                    isAdult
+                    coverImage {
+                      extraLarge
+                      large
+                      medium
+                    }
+                }
+      }
+    }`;
+
+    let variables = {
+      page: 1,
+      perPage: 6,
+    };
+
+    const result = await apiData
+      .getTitle({
+        query,
+        variables,
+      })
+      .catch((err) => console.log(err));
+
+    this.topManga = result.data.data.Page.media.filter((a) => {
+      return a.isAdult === false;
+    });
   },
 
   components: {
