f6ab0bfaa943bd8b12343b3633645eed9d621494
Ray0Emma <ahmadifarah22@gmail.com>
2022-01-29 12:50:40 +0000

    feat: added manga search result

--- src/views/SearchBar.vue
+++ src/views/SearchBar.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container mx-auto px-8">
-    <div class="mb-5">
-      {{ input }}
-    </div>
-    <Card :medias="searchResult" />
+    <div class="my-5">Anime Results for {{ input }}.</div>
+    <Card :medias="animeResult" />
+    <div class="my-5">Manga Results for {{ input }}.</div>
+    <Card :medias="mangaResult" />
   </div>
 </template>
 
@@ -15,13 +15,16 @@ export default {
   data() {
     return {
       input: this.$route.query.search,
-      searchResult: [],
+      totalResult: 0,
+      type: "ANIME",
+      animeResult: [],
+      mangaResult: [],
     };
   },
 
   async created() {
     const query = `
-        query ($id: Int, $page: Int, $perPage: Int, $search: String) {
+        query ($id: Int, $page: Int, $perPage: Int, $search: String, $type: MediaType) {
           Page(page: $page, perPage: $perPage) {
             pageInfo {
               total
@@ -30,7 +33,7 @@ export default {
               hasNextPage
               perPage
             }
-            media(id: $id, search: $search, sort: POPULARITY_DESC) {
+            media(id: $id, search: $search, type: $type, sort: POPULARITY_DESC) {
               id
               idMal
               title {
@@ -79,18 +82,36 @@ export default {
 
     let variables = {
       search: this.input,
+      type: this.type,
       page: 1,
       perPage: 6,
     };
 
-    const result = await apiData
+    let result = await apiData
       .getTitle({
         query,
         variables,
       })
       .catch((err) => console.log(err));
 
-    this.searchResult = result.data.data.Page.media;
+    this.totalResult = result.data.data.Page.media.length;
+    this.animeResult = result.data.data.Page.media;
+
+    variables = {
+      search: this.input,
+      type: "MANGA",
+      page: 1,
+      perPage: 6,
+    };
+
+    result = await apiData
+      .getTitle({
+        query,
+        variables,
+      })
+      .catch((err) => console.log(err));
+
+    this.mangaResult = result.data.data.Page.media;
   },
 
   components: {
