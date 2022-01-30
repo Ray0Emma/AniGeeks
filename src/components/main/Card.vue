7f3a6fad4c7d64821c6289f3c696aff71bdc6292
Ray0Emma <ahmadifarah22@gmail.com>
2022-01-30 21:43:44 +0000

    fix: fixed Card component UI

--- src/components/main/Card.vue
+++ src/components/main/Card.vue
@@ -1,37 +1,35 @@
 <template>
   <div
-    class="mt-12 grid lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-3 place-content-center"
+    class="mt-12 grid lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 place-content-center"
   >
     <div v-for="(media, index) in medias" :key="index">
-      <div>
-        <div
-          class="transform hover:scale-105 transition ease-in-out duration-500"
-        >
-          <a :href="media.siteUrl" target="_blank" rel="noopener noreferrer">
-            <img
-              :src="media.coverImage.large"
-              :alt="
-                media.title.english === null
-                  ? media.title.romaji
-                  : media.title.english
-              "
-              class="relative h-60 w-full object-cover object-top shadow-lg"
-            />
-          </a>
-          <p class="absolute left-1 bottom-1 text-xs text-yellow-500">
-            <Icon icon="fa-solid:star" class="inline mb-1 mr-2" />{{
-              media.averageScore
-            }}%
-          </p>
-        </div>
-        <p class="font-semibold text-gray-500 mt-3 mb-3">
-          {{
-            media.title.english === null
-              ? media.title.romaji
-              : media.title.english
-          }}
+      <div
+        class="transform hover:scale-105 transition ease-in-out duration-500"
+      >
+        <a :href="media.siteUrl" target="_blank" rel="noopener noreferrer">
+          <img
+            :src="media.coverImage.large"
+            :alt="
+              media.title.english === null
+                ? media.title.romaji
+                : media.title.english
+            "
+            class="relative h-60 w-full object-cover object-top shadow-xl rounded"
+          />
+        </a>
+        <p class="absolute left-1 bottom-1 text-xs text-yellow-500">
+          <Icon icon="fa-solid:star" class="inline mb-1 mr-2" />{{
+            media.averageScore
+          }}%
         </p>
       </div>
+      <p class="font-semibold text-gray-600 mt-3 mb-3 capitalize truncate">
+        {{
+          media.title.english === null
+            ? media.title.romaji
+            : media.title.english.toLowerCase()
+        }}
+      </p>
     </div>
   </div>
 </template>
