67cf3086f3300f0a8514c726e0454c8782952bc6
Ray0Emma <ahmadifarah22@gmail.com>
2022-01-30 19:14:16 +0000

    feat: installed and configured vueprogressBar

--- src/main.js
+++ src/main.js
@@ -3,5 +3,21 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./assets/style/tailwind.css";
+import VueProgressBar from "@aacassandra/vue3-progressbar";
 
-createApp(App).use(store).use(router).mount("#app");
+const options = {
+  color: "#e67c6c",
+  failedColor: "#b32229",
+  thickness: "5px",
+  transition: {
+    speed: "0.2s",
+    opacity: "0.6s",
+    termination: 300,
+  },
+};
+
+createApp(App)
+  .use(VueProgressBar, options)
+  .use(store)
+  .use(router)
+  .mount("#app");
