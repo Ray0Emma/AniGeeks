b1b69a152d7e103c8b53f1c295e2bc5190c30203
Ray0Emma <ahmadifarah22@gmail.com>
2022-02-12 09:36:05 +0000

    fix: fixed footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,41 @@
 <!-- eslint-disable -->
 <template>
-  <footer class="bg-white border-t-4 border-red-500 mt-9 inset-x-0 bottom-0">
-    <div
-      class="container flex flex-wrap items-center justify-center px-4 py-8 mx-auto lg:justify-between"
-    >
+  <footer class="bg-white mt-9 inset-x-0 bottom-0">
+    <div class="container flex items-center justify-center px-4 py-8 mx-auto">
       <div class="flex flex-wrap justify-center">
         <ul class="flex items-center space-x-4">
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-          <li>Terms</li>
+          <li>
+            <Icon
+              icon="entypo-social:github-with-circle"
+              :inline="true"
+              width="27"
+              height="27"
+            />
+          </li>
+          <li>
+            <Icon
+              icon="entypo-social:facebook-with-circle"
+              :inline="true"
+              width="27"
+              height="27"
+            />
+          </li>
+          <li>
+            <Icon
+              icon="entypo-social:twitter-with-circle"
+              :inline="true"
+              width="27"
+              height="27"
+            />
+          </li>
+          <li>
+            <Icon
+              icon="entypo-social:linkedin-with-circle"
+              :inline="true"
+              width="27"
+              height="27"
+            />
+          </li>
         </ul>
       </div>
     </div>
@@ -17,8 +43,12 @@
 </template>
 <!-- eslint-disable -->
 <script>
+import { Icon } from "@iconify/vue";
 /* eslint-disable */
 export default {
   name: "Footer",
+  components: {
+    Icon,
+  },
 };
 </script>
