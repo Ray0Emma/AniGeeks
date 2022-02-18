e55a67a84355ed271cfce73291a478ec3299727c
Ray0Emma <ahmadifarah22@gmail.com>
2022-02-18 23:31:33 +0000

    fix: fixed mediafullpage link

--- src/components/main/Card.vue
+++ src/components/main/Card.vue
@@ -7,26 +7,35 @@
         class="transform hover:scale-105 transition ease-in-out duration-500"
       >
         <!-- media link -->
-        <a :href="media.id">
-          <img
-            :src="media.coverImage.large"
-            :alt="
-              media.title.english === null
-                ? media.title.romaji
-                : media.title.english
-            "
-            class="relative h-60 w-full object-cover object-top shadow-xl rounded"
-          />
+        <!-- <a
+          :to="{
+            name: 'mediafullpage',
+            params: { type: mediaType, id: media.id },
+          }"
+        > -->
+        <img
+          :src="media.coverImage.large"
+          :alt="
+            media.title.english === null
+              ? media.title.romaji
+              : media.title.english
+          "
+          :to="{
+            name: 'mediafullpage',
+            params: { type: mediaType, id: media.id },
+          }"
+          class="relative h-60 w-full object-cover object-top shadow-xl rounded"
+        />
 
-          <p
-            v-if="media.averageScore"
-            class="absolute left-1 bottom-1 text-xs text-yellow-500"
-          >
-            <Icon icon="fa-solid:star" class="inline mb-1 mr-2" />{{
-              media.averageScore
-            }}%
-          </p>
-        </a>
+        <p
+          v-if="media.averageScore"
+          class="absolute left-1 bottom-1 text-xs text-yellow-500"
+        >
+          <Icon icon="fa-solid:star" class="inline mb-1 mr-2" />{{
+            media.averageScore
+          }}%
+        </p>
+        <!-- </a> -->
       </div>
       <p class="font-semibold text-gray-600 mt-3 mb-3 capitalize truncate">
         {{
