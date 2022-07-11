a1ee267f65d0205eb9b5218531c118230c3373aa
Ray0Emma <ahmadifarah22@gmail.com>
2022-07-11 17:22:26 +0000

    fix: fixed mediafullpage

--- src/views/MediaFullPage.vue
+++ src/views/MediaFullPage.vue
@@ -1,5 +1,49 @@
 <template>
   <div>
-    <h1>hi</h1>
+    <div v-for="(media, index) in medias" :key="index">
+      <img
+        :src="media.coverImage.large"
+        :alt="
+          media.title.english === null
+            ? media.title.romaji
+            : media.title.english
+        "
+        class="relative h-60 w-full object-cover object-top shadow-xl rounded"
+      />
+
+      <p
+        v-if="media.averageScore"
+        class="absolute left-1 bottom-1 text-xs text-black"
+      >
+        {{
+          media.title.english === null
+            ? media.title.romaji
+            : media.title.english
+        }}
+      </p>
+    </div>
   </div>
 </template>
+<script>
+// import { Icon } from "@iconify/vue";
+export default {
+  data() {
+    return {};
+  },
+  name: "MediaFullPage",
+  props: {
+    medias: Array,
+    mediaId: {
+      type: Number,
+      default: 0,
+      requred: true,
+    },
+    mediaType: {
+      type: String,
+    },
+  },
+  components: {
+    // Icon,
+  },
+};
+</script>
